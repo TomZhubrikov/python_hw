1d768dd12c03b7b886c012ace229532cef0022de
Tom <artzhubrikov@mail.ru>
2023-02-08 12:02:56 +0300

    HW-4 is completed

--- HW4/Task_1.py
+++ HW4/Task_1.py
@@ -4,12 +4,22 @@
 # Затем пользователь вводит сами элементы множеств.
 
 # Пример:
-# 11 6
 # 2 4 6 8 10 12 10 8 6 4 2
 # 3 6 9 12 15 18
 # 6 12
 
-size_1 = int(input("Enter a size of first set -> "))
-size_2 = int(input("Enter a size of second set -> "))
+# (без ввода кол-ва элемантов множеств)
+set_1 = input("Enter a size of first set -> ")
+set_2 = input("Enter a size of second set -> ")
 
-for
+set_1 = set(set_1.split())
+set_2 = set(set_2.split())
+set_3 = set_1.intersection(set_2)
+list_set = [int(num) for num in list(set_3)]
+list_set.sort()
+list_set = [str(num) for num in list_set]
+
+res_str = f"Result -> {' '.join(list_set)}"
+print(res_str)
+
+
--- HW4/Task_2.py
+++ HW4/Task_2.py
@@ -5,3 +5,14 @@
 # Пример:
 # 4 -> 1 2 3 4
 # 9
+
+bushes = [int(num) for num in input("Введите массив чисел -> ").split()]
+
+max_barry = -1
+for i in range(len(bushes)):
+    if i == len(bushes) - 1:
+        if bushes[i - 1] + bushes[i] + bushes[0] > max_barry:
+            max_barry = bushes[i - 1] + bushes[i] + bushes[0]
+    elif bushes[i - 1] + bushes[i] + bushes[i + 1] > max_barry:
+        max_barry = bushes[i - 1] + bushes[i] + bushes[i + 1]
+print(max_barry)
